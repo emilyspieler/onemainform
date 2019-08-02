d9a91612099a321f4936ce4332e0957ebb053831
Emily Spieler <emilyspieler1@gmail.com>
2019-08-01 19:15:13 -0500

    add desktop wireframe version

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-//credit and debit card selector feature
-$(document).ready(function(){
-    $('#cardtype').on('change', function() {
-      if ( this.value == '1')
-      {
-        $("#credit").show();
-        $("#debit").hide();
-      }
-      else if ( this.value == '2')
-      {
-        $("#credit").hide();
-        $("#debit").show();
-      }
-      else {
-        $("#debit").hide();
-        $("#credit").hide();
-      }
-    });
+//chart number one function
+$(function () {
+    $(".checking").show();
+    //if you have any radio selected by default
+    $('[name="optionsRadios"]:checked').addClass('selected');
 });
+  $(document).on('click', '[name="optionsRadios"]', function () {
+      if (!$(this).hasClass('selected')) {
+          var $selected = $('.selected[name="optionsRadios"]');
+          if ($selected.length != 0){
+            if ( $selected.val() == 1)
+                  {
+                    $(".checking").hide();
+                    $(".debit").show();
+                  }
+              else {
+                $(".checking").show();
+                $(".debit").hide();
+              }
 
-//min expiration date validation
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
- if(dd<10){
-        dd='0'+dd
-    }
-    if(mm<10){
-        mm='0'+mm
-    }
+              $selected.removeClass('selected');
+          }
+      }
+      $(this).addClass('selected');
+  });
+//password verification
+var number = document.getElementById("accountnumber1")
+  , confirm_number = document.getElementById("accountnumber2");
 
-today = yyyy+'-'+mm+'-'+dd;
-document.getElementById("datefield").setAttribute("min", today);
-document.getElementById("datefieldcredit").setAttribute("min", today);
-
-//validate selection between debit and credit
-function validateForm() {
-  var selection = document.forms["applicationform"]["cardtype"].value;
-
-  if (value === "") {
-    alert("no");
-    return false;
+function validateNumber(){
+  if(number.value !== confirm_number.value) {
+    confirm_number.setCustomValidity("Account numbers do not match");
+  } else {
+    confirm_number.setCustomValidity('');
   }
-
 }
+
+number.onchange = validateNumber;
+confirm_number.onkeyup = validateNumber;
